f3c0d2b9ac4ef9640f75e89261a735b822da18b1
Victor Englund <victor.englund@gmail.com>
2015-09-18 19:08:39 +0200

    Restructured tables

--- sql/products.sql
+++ sql/products.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS `items` (
+CREATE TABLE IF NOT EXISTS `products` (
   `id` int(11) PRIMARY KEY AUTO_INCREMENT NOT NULL,
   `name` varchar(255) NOT NULL,
-  `description` varchar(255) DEFAULT NULL,
   `price` decimal(10,0) NOT NULL,
+  `quantity` smallint UNSIGNED NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
--- sql/users.sql
+++ sql/users.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(32) PRIMARY KEY NOT NULL,
   `password` varchar(128) NOT NULL,
   `salt` varchar(8) NOT NULL,
-  `address` varchar(255) DEFAULT NULL
+  `address` varchar(255) DEFAULT NULL,
+  `is_admin` tinyint UNSIGNED NOT NULL DEFAULT 0
 );
